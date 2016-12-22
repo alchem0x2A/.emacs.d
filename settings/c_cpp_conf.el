f91773876a3c5ebfbcc52e018e9da69b1ec38539
T.Tian <tian.tian@chem.ethz.ch>
2016-12-22 19:33:09 +0100

    Added electric pairing in c++

--- settings/c_cpp_conf.el
+++ settings/c_cpp_conf.el
@@ -2,6 +2,7 @@
 (add-hook 'c++-mode-hook 'irony-mode)
 (add-hook 'c-mode-hook 'irony-mode)
 (add-hook 'objc-mode-hook 'irony-mode)
+(add-hook 'c++-mode-hook 'electric-pair-mode)
 
 ;; replace the `completion-at-point' and `complete-symbol' bindings in
 ;; irony-mode's buffers by irony-mode's function
