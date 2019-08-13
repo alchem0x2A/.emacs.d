a5fa09daa4b68dbf6d9d6f9ce2e43257afeb3e1d
T.Tian <lovaulonzezoeta@gmail.com>
2019-08-13 16:06:32 +0200

    Add support for LaTeX files. Supporting XeLaTeX + LaTeXmk as default

--- settings/latex_conf.el
+++ settings/latex_conf.el
@@ -1,4 +1,19 @@
 ;; Configuration for LATEX files
-(load "auctex.el" nil t t)
-;; Use RefTeX
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq TeX-PDF-mode t)
+;; Will ask fo master file each time?
+(setq-default TeX-master nil)
+
+(add-hook 'LaTeX-mode-hook 'visual-line-mode)
+(add-hook 'LaTeX-mode-hook 'flyspell-mode)
+(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
 (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(add-hook 'LaTeX-mode-hook 'cdlatex-mode)
+(setq reftex-plug-into-AUCTeX t)
+
+;; Enable choosing XeLaTeX and LuaTeX as TeX engining as well as Latexmk
+(setq TeX-check-engine t)
+(auctex-latexmk-setup)
+;; Useful when invoking sources made by org mode
+(setq-default TeX-command-extra-options "-shell-escape")
